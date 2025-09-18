d61db85a68803400a0be0f7eb5ca3ae5fd46078a
Raf4Janni <rsjannini@gmail.com>
2025-09-18 14:39:59 -0300

    feat: mensagem para o /start

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 
 import handlers.fronts
+import handlers.links
 
 # Loading dotenv data
 load_dotenv()
@@ -17,8 +18,10 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start']) 
 def start(msg: telebot.types.Message):
-    bot.send_message(msg.chat.id, )
+    bot.send_message(msg.chat.id, "Eu sou o bot do CodeLab")
 
 handlers.fronts.show_fronts(bot)
+
+handlers.links.show_links(bot)
 
 bot.infinity_polling()
