c6d03a539659181a4f80e8dfd9b992263593f0d1
Raf4Janni <rsjannini@gmail.com>
2025-10-08 13:49:28 -0300

    feat: criacao dos links

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import telebot
 from dotenv import load_dotenv
 import os
 
-from handlers import fronts, checkin, setCommands, codelab #,links
+from handlers import fronts, checkin, setCommands, codelab ,links
 from handlers.codelab import CodelabHandler
 from dependencies.internal import dados_checkin
 # Carregando as chaves no .env
@@ -25,6 +25,7 @@ def create_bot(TOKEN):
 
     # Injetando as dependências nas features
     codelab_comm = codelab.CodelabHandler(bot, CODELAB_NAME_LIST)
+    link = links.show_links(bot)
 
     checkin_main = checkin.main_checkin(bot, DATABASE=checkin_DB)
     checkin_add = checkin.add_checkin(bot, DATABASE=checkin_DB)
@@ -34,6 +35,8 @@ def create_bot(TOKEN):
 
     # Composição das featrues no bot
     bot.register_message_handler(codelab_comm, commands=['codelab'])
+
+    bot.register_message_handler(link,commands=['links'])
 
     bot.register_message_handler(checkin_main, commands=['checkin'])
     bot.register_message_handler(checkin_add, commands=['checkin_add'])
---
+++ src/handlers/links.py
@@ -0,0 +1,30 @@
+import telebot
+from handlers.abstract import msg_handler
+
+class show_links(msg_handler):
+
+    def __init__(self,bot):
+        super().__init__(bot)
+
+
+    def __call__(self,msg: telebot.types.Message):
+        links = """
+        🧪Aqui estão alguns links úteis do CodeLab🧪:
+         - 😺 Github: https://github.com/USPCodeLabSanca
+
+         - 💻 Notion Dev-Boost (apenas membros do notion Codelab): 
+         https://www.notion.so/Dev-Boost-a93325cc05ca43059773d0e6850d6037
+
+         - 📚 Notion Dev-Learn (apenas membros do notion Codelab): 
+         https://www.notion.so/Dev-Learn-b9a3cb1804f748c1bfc91ce2c9519dea
+
+         - 🎉 Notion Dev-Hack (apenas membros do notion Codelab): https://www.notion.so/Dev-Hack-1c63571b691a4c55955d872e2245e5f4
+
+         - 🍽 Notion Dev-Clara(apenas membros do notion Codelab): https://www.notion.so/Dev-Clara-1df8def36c1680c1bc6ce1145c5a3026
+
+         - 📷 Instagram: https://www.instagram.com/uspcodelabsanca/
+
+         - 🌐 Site: https://codelab.icmc.usp.br/
+        """
+
+        self.BOT.send_message(msg.chat.id,links,message_thread_id=msg.message_thread_id)
