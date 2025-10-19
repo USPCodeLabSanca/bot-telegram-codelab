a514c28b33eb56359e2a25e428cb0e200ec578a3
joaopneves1570 <bolotaneves@gmail.com>
2025-10-19 11:51:13 -0300

    Implementando fronts assíncrono

--- src/handlers/fronts.py
+++ src/handlers/fronts.py
@@ -5,11 +5,11 @@ from handlers.abstract import msg_handler
 
 class ShowFronts(msg_handler):
     def __init__(self, bot):
-        super().__init__(self, bot)
+        super().__init__(bot)
         self.callback()
         pass
 
-    def __call__(self, msg: telebot.types.Message):
+    async def __call__(self, msg: telebot.types.Message):
         fronts = 'No CodeLabSanca🧪, temos cinco frentes principais:\n📚 Dev. Learn\n💻 Dev. Boost\n🎉 Dev. Hack.\n🥚 Dev. Clara\n🙅‍♂️ Dev. Hire \n\n\n\n👉 Sobre qual você quer saber?'
 
         markup = types.InlineKeyboardMarkup()
@@ -22,25 +22,25 @@ class ShowFronts(msg_handler):
 
         markup.add(learn_button, boost_button, hack_button, clara_button, hire_button)
 
-        self.BOT.send_message(msg.chat.id, fronts, reply_markup=markup, message_thread_id=msg.message_thread_id)
+        await self.BOT.send_message(msg.chat.id, fronts, reply_markup=markup, message_thread_id=msg.message_thread_id)
 
     def callback(self):
         @self.BOT.callback_query_handler(func= lambda call: call.data.startswith('dev'))
-        def resposta_botao(call:types.CallbackQuery):
+        async def resposta_botao(call:types.CallbackQuery):
 
             topic = call.message.message_thread_id
 
             match call.data:
                 case 'devlearn_button':
-                    self.BOT.send_message(call.message.chat.id, '📚 O *Dev\\. Learn* é a frente mais educativa do grupo\\.\nSão os integrantes responsáveis por ministrar *diferentes cursos em diversas tecnologias*, para estudantes tanto de *dentro* quanto de *fora* da *USP* 🏫\\.', parse_mode="MarkdownV2", message_thread_id=topic)
+                    await self.BOT.send_message(call.message.chat.id, '📚 O *Dev\\. Learn* é a frente mais educativa do grupo\\.\nSão os integrantes responsáveis por ministrar *diferentes cursos em diversas tecnologias*, para estudantes tanto de *dentro* quanto de *fora* da *USP* 🏫\\.', parse_mode="MarkdownV2", message_thread_id=topic)
                 case 'devboost_button':
-                    self.BOT.send_message(call.message.chat.id, '💻 O *Dev\\. Boost* é a frente mais técnica do grupo\\.\nO foco principal da frente é desenvolver *projetos práticos* através da elaboração de sub\\-grupos onde diversas tecnologias são *praticadas* e *implementadas* 🧪\\.', parse_mode="MarkdownV2",  message_thread_id=topic)
+                    await self.BOT.send_message(call.message.chat.id, '💻 O *Dev\\. Boost* é a frente mais técnica do grupo\\.\nO foco principal da frente é desenvolver *projetos práticos* através da elaboração de sub\\-grupos onde diversas tecnologias são *praticadas* e *implementadas* 🧪\\.', parse_mode="MarkdownV2",  message_thread_id=topic)
                 case 'devhack_button':
-                    self.BOT.send_message(call.message.chat.id, '🎉 O *Dev\\. Hack* é a frente mais criativa do grupo\\.\nÉ o grupo responsável por _idealizar_, _organizar_ e _implementar_ *hackatons*\\.\nSão os membros na função de pensar em ideias criativas e dinâmicas para tornar esses eventos *divertidos* e *bem organizados* 🍕\\.', parse_mode="MarkdownV2",  message_thread_id=topic)
+                    await self.BOT.send_message(call.message.chat.id, '🎉 O *Dev\\. Hack* é a frente mais criativa do grupo\\.\nÉ o grupo responsável por _idealizar_, _organizar_ e _implementar_ *hackatons*\\.\nSão os membros na função de pensar em ideias criativas e dinâmicas para tornar esses eventos *divertidos* e *bem organizados* 🍕\\.', parse_mode="MarkdownV2",  message_thread_id=topic)
                 case 'devclara_button':
-                    self.BOT.send_message(call.message.chat.id, '🥚 O *Dev\\. Clara* é a frente focada em resolver *questões técnicas* dadas em _entrevistas de emprego_ treinando e incentivando os membros a desenvolver a solução e explicar o que está sendo feito\n 🧑‍💼Tudo isso com a ajuda de outros membros\\.', parse_mode="MarkdownV2",  message_thread_id=topic)
+                    await self.BOT.send_message(call.message.chat.id, '🥚 O *Dev\\. Clara* é a frente focada em resolver *questões técnicas* dadas em _entrevistas de emprego_ treinando e incentivando os membros a desenvolver a solução e explicar o que está sendo feito\n 🧑‍💼Tudo isso com a ajuda de outros membros\\.', parse_mode="MarkdownV2",  message_thread_id=topic)
                 case 'devhire_button':
-                    self.BOT.send_message(call.message.chat.id, '🙅‍♂️ O *Dev\\. Hire* é a frente na qual postamos *dicas* sobre _empregos e oportunidades_\\.\n Todos os membros estão automaticamente nessa frente 😁\\!', parse_mode = "MarkdownV2",  message_thread_id=topic)
+                    await self.BOT.send_message(call.message.chat.id, '🙅‍♂️ O *Dev\\. Hire* é a frente na qual postamos *dicas* sobre _empregos e oportunidades_\\.\n Todos os membros estão automaticamente nessa frente 😁\\!', parse_mode = "MarkdownV2",  message_thread_id=topic)
 
         
 
